f97987501c5df0b447ad8e3bbad9a4d9a3f7ed8a
Sooin Hwang <Sooin Hwang@DESKTOP-QQO099C>
2022-01-18 11:16:07 +0900

    Added Account Class which contains bank user account information.

---
+++ src/exercise/Account.java
@@ -0,0 +1,44 @@
+package exercise;
+
+public class Account {
+
+	// 멤버 변수 정의
+	private String accountID;
+	private String accountPassword;
+	private String accountName;
+	private int accountBalance;
+
+	// get, set 메소드
+
+	public String getAccountID() {
+		return accountID;
+	}
+
+	public void setAccountID(String accountID) {
+		this.accountID = accountID;
+	}
+
+	public String getAccountPassword() {
+		return accountPassword;
+	}
+
+	public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public int getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+}
